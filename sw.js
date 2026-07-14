9c8f457d29851bdaf05aa1344b360688f10e22d4
golpy20-creator <golpy20@gmail.com>
2026-07-14 14:15:35 +0400

    Add service worker with caching functionality
    Implement service worker for caching assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = "tamagotchi-cat-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+  // сюда позже можно добавить свои иконки и другие файлы
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
